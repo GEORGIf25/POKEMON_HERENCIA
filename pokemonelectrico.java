114307ac00f140c8b595b0b2e28818a00a75f7bc
JORGE <132015753+GEORGIf25@users.noreply.github.com>
2023-10-06 08:55:47 -0600

    Create pokemonelectrico.java

---
+++ pokemonelectrico.java
@@ -0,0 +1,15 @@
+ */
+package herencia;
+
+/**
+ *
+ * @author Alumno-87
+ */
+public class pokemonElectrico extends pokemon{
+    public pokemonElectrico(String nombre, int nivel, String tipo){
+        super(nombre,tipo,nivel);
+    }
+}
+
+        
+    
